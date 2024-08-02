e23589f1703705f26ccb3e685ac0cd3a1e186467
LeeMaretele <leratomaretele81@gmail.com>
2024-08-02 19:23:27 +0200

    Added action to the delete buttons and the add section

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -17,10 +17,26 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function AddTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
       }}
     >
       {children}
